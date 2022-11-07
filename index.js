0feba95021bf811cb3f6b59942e53e5c0cf78199
FAForhad <faforhad450@gmail.com>
2022-11-08 03:05:51 +0600

    get_services

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -6,6 +7,29 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c3txqlb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('wayneServices').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const quary = {};
+            const cursor = serviceCollection.find(quary);
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send('Wanye server is running')
