225efbaf565e64c8b884c62d49a788c8fb92cd38
FAForhad <faforhad450@gmail.com>
2022-11-08 01:55:44 +0600

    server-is-running

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Wanye server is running')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`app is running in port ${port}`)
+})
